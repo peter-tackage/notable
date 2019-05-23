4b84e94f8e3e586fe236604e3a987db55bbf7236
Peter Tackage <peter.tackage@futurice.com>
2019-05-23 10:25:19 +0200

    Add slightly ugly ButtonBar. TextNote widget still needs fixes.

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,20 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notable/bloc/feed/feed_bloc.dart';
 import 'package:notable/bloc/feed/feed_events.dart';
-import 'package:notable/bloc/notes/notes.dart';
-import 'package:notable/entity/entity.dart';
-import 'package:notable/model/checklist.dart';
-import 'package:notable/model/text_note.dart';
 import 'package:notable/screen/checklist_note_screen.dart';
+import 'package:notable/screen/text_note_screen.dart';
 import 'package:notable/widget/notes_page.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
 
   final String title;
-
-  // Function used to initial the data source
-  // final void Function() onInit;
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -24,41 +18,46 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   FeedBloc _feedBloc;
 
-  void _openNoteEditor(BuildContext context) => Navigator.push(
-      context,
-      MaterialPageRoute(
-          builder: (context) => AddEditChecklistNoteScreen(id: null)));
-
   @override
   void initState() {
     super.initState();
-
-    _feedBloc = FeedBloc(
-        textNotesBloc:
-            BlocProvider.of<NotesBloc<TextNote, NoteEntity>>(context),
-        checklistNotesBloc:
-            BlocProvider.of<NotesBloc<Checklist, ChecklistEntity>>(context));
-    _feedBloc.dispatch(LoadFeed());
-    //  widget.onInit();
+    _feedBloc = BlocProvider.of<FeedBloc>(context);
+    _feedBloc.dispatch(LoadFeed()); // FIXME Should be moved out
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-          padding: EdgeInsets.all(16),
-          child: BlocProvider<FeedBloc>(
-            bloc: _feedBloc,
-            child: NotesPage(),
-          )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _openNoteEditor(context),
-        tooltip: 'Add note',
-        child: Icon(Icons.add),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: BlocProvider<FeedBloc>(
+              bloc: _feedBloc,
+              child: AllNotesPage(),
+            )),
+            Container(
+              color: Colors.grey[200],
+              child: ButtonBar(children: <Widget>[
+                IconButton(
+                    onPressed: () => _openTextNoteEditor(context),
+                    icon: Icon(Icons.note_add)),
+                IconButton(
+                    onPressed: () => _openChecklistEditor(context),
+                    icon: Icon(Icons.playlist_add))
+              ]),
+            ),
+          ],
+        ));
   }
+
+  void _openChecklistEditor(BuildContext context) => Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => AddEditChecklistNoteScreen(id: null)));
+
+  void _openTextNoteEditor(BuildContext context) => Navigator.push(context,
+      MaterialPageRoute(builder: (context) => AddEditTextNoteScreen(id: null)));
 }
