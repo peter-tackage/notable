0998503c1c42120dc0b0d86ec7e3ac0edec0b834
peter <peter.tackage@gmail.com>
2021-03-08 19:55:53 +0100

    Get some blocTests working
    Not sure I really like this approach because it's hard to refer to Fixtures you create when doing the verify.

--- test/bloc/notes/notes_bloc_test.dart
+++ test/bloc/notes/notes_bloc_test.dart
@@ -1,5 +1,5 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:built_collection/built_collection.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:notable/bloc/notes/notes.dart';
 import 'package:notable/data/repository.dart';
@@ -7,98 +7,104 @@ import 'package:notable/entity/entity.dart';
 import 'package:notable/model/label.dart';
 import 'package:notable/model/text_note.dart';
 import 'package:notable/model/text_note_mapper.dart';
-import 'package:uuid/uuid.dart';
+import 'package:test/test.dart';
 
 class MockRepository extends Mock implements Repository<TextNoteEntity> {}
 
 void main() {
-  MockRepository noteRepository;
-  var textNoteBloc =
-      NotesBloc<TextNote, TextNoteEntity>(
-          noteRepository: noteRepository, mapper: TextNoteMapper());
-
-  setUp(() {
-    // No value in mocking the Mapper, use the real one.
-    noteRepository = MockRepository();
-    textNoteBloc = NotesBloc<TextNote, TextNoteEntity>(
-        noteRepository: noteRepository, mapper: TextNoteMapper());
-  });
+  final _dateNow = DateTime.now();
 
   test('initial state is NoteLoading', () {
-    Stream<NotesState> stateStream = textNoteBloc;
-
-    expect(
-        stateStream,
-        emitsInOrder([
-          isA<NotesLoading>(),
-        ]));
+    final textNoteBloc = NotesBloc<TextNote, TextNoteEntity>(
+        noteRepository: MockRepository(), mapper: TextNoteMapper());
+    expect(textNoteBloc.state, isA<NotesLoading>());
   });
 
-  test('LoadNotes triggers NotesLoading, NotesLoaded with value', () {
-    // given
-    final title = 'The note title';
-    final text = 'The note text';
-    final id = Uuid().v1().toString();
-    final datetime = DateTime.now();
+  // test('initial state is NoteLoading', () {
+  //
+  //   noteRepository = MockRepository();
 
-    var noteEntity = TextNoteEntity(<LabelEntity>[], title, text,
-        id: id, updatedDate: datetime);
-    when(noteRepository.getAll()).thenAnswer((_) => Future.value([noteEntity]));
+  blocTest<NotesBloc<TextNote, TextNoteEntity>, NotesState>(
+    'emits [NotesLoading] on LoadNotes',
+    build: () => NotesBloc<TextNote, TextNoteEntity>(
+        noteRepository: MockRepository(), mapper: TextNoteMapper()),
+    act: (a) => {a.add(LoadNotes())},
+    expect: [NotesLoading()],
+  );
 
-    // when
-    textNoteBloc.add(LoadNotes());
+  blocTest<NotesBloc<TextNote, TextNoteEntity>, NotesState>(
+    'emits nothing when no action',
+    build: () => NotesBloc<TextNote, TextNoteEntity>(
+        noteRepository: MockRepository(), mapper: TextNoteMapper()),
+    act: (a) => {},
+    expect: [],
+  );
 
-    // then
-    expect(
-        textNoteBloc.state,
-        emitsInOrder([
-          isA<NotesLoading>(),
-          equals(NotesLoaded([
-            TextNote((b) => b
-              ..title = title
-              ..labels = ListBuilder<Label>()
-              ..text = text
-              ..id = id
-              ..updatedDate = datetime)
-          ]))
-        ]));
-  });
+  blocTest<NotesBloc<TextNote, TextNoteEntity>, NotesState>(
+    'Load [NotesLoading] on LoadNotes',
+    build: () {
+      final title = 'The note title';
+      final text = 'The note text';
+      final id = 'the id';
+      final datetime = _dateNow;
+      var noteEntity = TextNoteEntity(<LabelEntity>[], title, text,
+          id: id, updatedDate: datetime);
 
-  test('AddNote triggers NotesLoading, NotesLoaded with value', () {
-    // given
+      final noteRepository = MockRepository();
+      when(noteRepository.getAll())
+          .thenAnswer((_) => Future.value([noteEntity]));
 
-    // This will be the returned "saved" entity from the repository
-    final title = 'The note title';
-    final text = 'The note text';
+      return NotesBloc<TextNote, TextNoteEntity>(
+          noteRepository: noteRepository, mapper: TextNoteMapper());
+    },
+    act: (a) => {a.add(LoadNotes())},
+    expect: [
+      NotesLoading(),
+      NotesLoaded([
+        TextNote((b) => b
+          ..title = 'The note title'
+          ..labels = ListBuilder<Label>()
+          ..text = 'The note text'
+          ..id = 'the id'
+          ..updatedDate = _dateNow)
+      ])
+    ],
+  );
 
-    // These properties are actually defined by the (real) repository.
-    final id = Uuid().v1().toString();
-    final datetime = DateTime.now();
-
-    var noteEntity = TextNoteEntity(<LabelEntity>[], title, text,
-        id: id, updatedDate: datetime);
-    when(noteRepository.getAll()).thenAnswer((_) => Future.value([noteEntity]));
-
-    var textNote = TextNote((b) => b
-      ..title = title
-      ..text = text);
-
-    // when
-    textNoteBloc.add(AddNote(textNote));
-
-    // then
-    expect(
-        textNoteBloc.state,
-        emitsInOrder([
-          isA<NotesLoading>(),
-          equals(NotesLoaded([
-            TextNote((b) => b
-              ..title = title
-              ..labels = ListBuilder<Label>()
-              ..text = text
-              ..id = id
-              ..updatedDate = datetime)
-          ]))
-        ]));
-  });
+  // test('AddNote triggers NotesLoading, NotesLoaded with value', () {
+  //   // given
+  //   // This will be the returned "saved" entity from the repository
+  //   final title = 'The note title';
+  //   final text = 'The note text';
+  //
+  //   // These properties are actually defined by the (real) repository.
+  //   final id = Uuid().v1().toString();
+  //   final datetime = DateTime.now();
+  //
+  //   var noteEntity = TextNoteEntity(<LabelEntity>[], title, text,
+  //       id: id, updatedDate: datetime);
+  //   when(noteRepository.getAll()).thenAnswer((_) => Future.value([noteEntity]));
+  //
+  //   var textNote = TextNote((b) => b
+  //     ..title = title
+  //     ..text = text);
+  //
+  //   // when
+  //   textNoteBloc.add(AddNote(textNote));
+  //
+  //   // then
+  //   expect(
+  //       textNoteBloc.state,
+  //       emitsInOrder([
+  //         isA<NotesLoading>(),
+  //         equals(NotesLoaded([
+  //           TextNote((b) => b
+  //             ..title = title
+  //             ..labels = ListBuilder<Label>()
+  //             ..text = text
+  //             ..id = id
+  //             ..updatedDate = datetime)
+  //         ]))
+  //       ]));
+  // });
 }
